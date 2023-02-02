bcae34f2180b9985c110baf0d34f5f667bdd5a05
Jeremiah Oyebamiji <phemmy09@gmail.com>
2023-02-02 15:47:02 +0100

    implement chack address type

--- main.go
+++ main.go
@@ -2,25 +2,48 @@ package main
 
 import (
     "context"
-    "fmt"
     "github.com/ethereum/go-ethereum/ethclient"
     "log"
+	"github.com/ethereum/go-ethereum/common"
+    "bufio"
+	"fmt"
+	"os"
+    "regexp"
+    "strings"
 )
-
-var (
-    ctx         = context.Background()
-    url         = "https://mainnet.infura.io/v3/aada9accef8a41fd8f503b051c6ed8bc"
-    client, _ = ethclient.DialContext(ctx, url)
-)
-
-func currentBlock() {
-    block, err := client.BlockByNumber(ctx, nil)
-    if err != nil {
-        log.Println(err)
-    }
-    fmt.Println(block.Number())
-}
 
 func main() {
-	currentBlock()
+    client, err := ethclient.Dial("https://mainnet.infura.io/v3/aada9accef8a41fd8f503b051c6ed8bc")
+	if err != nil {
+		fmt.Println("Error connecting to Ethereum node:", err)
+		return
+	}
+    re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+    
+    fmt.Print("Enter your wallet address: ")
+	reader := bufio.NewReader(os.Stdin)
+	inputData, _ := reader.ReadString('\n')
+    address := common.HexToAddress(strings.TrimSpace(inputData))
+
+    match := re.MatchString(address.String())
+
+    addressT := common.HexToAddress(address.String())
+	if addressT == (common.Address{}) || !match {
+		fmt.Println("Invalid address:", inputData)
+        os.Exit(1)
+	}
+
+    // Retrieve the code associated with the address
+	code, err := client.CodeAt(context.Background(), address, nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Check the length of the code
+	if len(code) > 0 {
+        fmt.Println("Address is a contract address", address)
+    } else {
+        fmt.Println("Address is an EOA", address)
+	}
 }
